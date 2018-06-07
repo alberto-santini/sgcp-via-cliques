b5010f647d56c6dd20cbed2a951c74126c332756
Alberto Santini <santini.alberto@gmail.com>
2018-06-07 14:48:34 +0200

    Refactored to use ProgramOptions

---
+++ FindPO.cmake
@@ -0,0 +1,23 @@
+find_path(PO_INCLUDE_DIR
+    NAMES po/ProgramOptions.hxx
+    PATHS ${PO_ROOT_DIR}
+    PATH_SUFFIXES include
+)
+
+mark_as_advanced(PO_FOUND PO_INCLUDE_DIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(PO
+    REQUIRED_VARS PO_INCLUDE_DIR
+)
+
+if(PO_FOUND)
+    set(PO_INCLUDE_DIRS ${PO_INCLUDE_DIR})
+endif()
+
+if(PO_FOUND AND NOT TARGET PO::PO)
+    add_library(PO::PO INTERFACE IMPORTED)
+    set_target_properties(PO::PO PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${PO_INCLUDE_DIR}"
+    )
+endif()
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "graph.h"
 #include <as/graph.h>
 #include <as/max_clique.h>
+#include <as/and_die.h>
+#include <po/ProgramOptions.hxx>
 #include <fstream>
 #include <chrono>
 #include <experimental/filesystem>
@@ -14,9 +16,51 @@ namespace fs = std::experimental::filesystem;
 int main(int argc, char* argv[]) {
     using namespace sgcp_cliques;
     using namespace std::chrono;
+    using as::and_die;
+    namespace fs = std::experimental::filesystem;
 
-    if(argc == 3) {
-        const auto cgraph = read_clustered_graph(argv[1]);
+    po::parser parser;
+
+    parser["graph"]
+        .abbreviation('g')
+        .description("File containing the graph. Mandatory.")
+        .type(po::string);
+
+    parser["output"]
+        .abbreviation('o')
+        .description("File where we append the results. Mandatory.")
+        .type(po::string);
+
+    parser["help"]
+        .abbreviation('h')
+        .description("Prints this help text.")
+        .callback([&] () { std::cout << parser << "\n"; });
+
+    parser(argc, argv);
+
+    if(!parser["graph"].was_set()) {
+        std::cerr << "You need to specify a graph file!" << and_die();
+    }
+
+    if(!parser["output"].was_set()) {
+        std::cerr << "You need to specify an output file!" << and_die();
+    }
+
+    std::string graph_file = parser["graph"].get().string;
+    std::string output_file = parser["output"].get().string;
+
+    if(!fs::exists(graph_file)) {
+        std::cerr << "Graph file does not exist: " << graph_file << and_die();
+    }
+
+    std::ofstream ofs;
+    ofs.open(output_file, std::ios_base::app);
+
+    if(ofs.fail()) {
+        std::cerr << "Cannot access output file: " << output_file << and_die();
+    }
+
+    const auto cgraph = read_clustered_graph(graph_file);
         const auto working_graph = as::graph::complementary(cgraph);
         const auto clique_graph = complementary_sandwich_line_graph(working_graph);
 
@@ -24,11 +68,7 @@ int main(int argc, char* argv[]) {
         const auto max_clique = as::max_clique::solve_with_mip(clique_graph);
         const auto end_time = high_resolution_clock::now();
         const auto elapsed = duration_cast<duration<float>>(end_time - start_time).count();
-        const auto chromatic_n = cgraph[boost::graph_bundle] - max_clique.size();
-        const auto instance = fs::path{argv[1]}.stem().string();
-
-        std::ofstream ofs;
-        ofs.open(argv[2], std::ios_base::app);
+    const auto instance = fs::path{graph_file}.stem().string();
 
         if(!ofs.fail()) {
             ofs << instance << "," << chromatic_n << "," << elapsed << "\n";
