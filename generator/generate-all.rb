5a40ab6b122b00e73492f1b90f9678fa2bb176d1
Alberto Santini <santini.alberto@gmail.com>
2018-10-15 16:00:48 +0200

    Added larger weighted graphs

--- generator/generate-all.rb
+++ generator/generate-all.rb
@@ -1,6 +1,6 @@
 require_relative 'generate-sgcp'
 
-(10 .. 100).step(10) do |vertices|
+(200 .. 500).step(100) do |vertices|
     (0.1 .. 0.9).step(0.1) do |density|
         (0.05 .. 0.3).step(0.05) do |part_sz|
             f = "graphs/weighted/wg-#{vertices}-#{density.round(1)}-#{part_sz.round(2)}.txt"
