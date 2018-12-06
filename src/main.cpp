caae46df07cc6771f057543a47dd6be10aef78cc
Alberto Santini <santini.alberto@gmail.com>
2018-12-06 17:52:27 +0100

    Added possibility to solve the clique version as a MWSS problem with exactcolors

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <as/graph.h>
 #include <as/max_clique.h>
 #include <as/and_die.h>
+#include <as/mwis.h>
 #include <ProgramOptions.hxx>
 #include <fstream>
 #include <chrono>
@@ -114,15 +115,16 @@ int main(int argc, char* argv[]) {
         const auto working_graph = as::graph::complementary(cwgraph);
         const auto clique_graph = smwgcp_cliques::complementary_sandwich_line_graph(working_graph);
 
-        std::cout << "Max-Weight Clique graph ready (" << duration_cast<duration<float>>(high_resolution_clock::now() - zero_time).count() << " s)\n";
-        std::cout << "Launching the Max-Weight Clique solver...\n";
+        std::cout << "Max-Weight Clique graph ready (" << duration_cast<duration<float>>(high_resolution_clock::now() - zero_time).count() << " s) ";
+        std::cout << boost::num_vertices(clique_graph) << " vertices and " << boost::num_edges(clique_graph) << " edges\n";
+        std::cout << "Launching the Max-Weight Clique solver (MIP)...\n";
 
         const auto start_time = high_resolution_clock::now();
         const auto max_clique_sol = as::max_clique::solve_with_mip(clique_graph, cplex_timeout);
         const auto end_time = high_resolution_clock::now();
         const auto elapsed = duration_cast<duration<float>>(end_time - start_time).count();
 
-        std::cout << "Clique solver finished (" << elapsed << " s)\n";
+        std::cout << "MIP Clique solver finished (" << elapsed << " s)\n";
 
         const auto weighted_chromatic_n_lb = smwgcp_cliques::sum_of_weights(cwgraph) - max_clique_sol.ub;
         const auto weighted_chromatic_n_ub = smwgcp_cliques::sum_of_weights(cwgraph) - max_clique_sol.lb;
@@ -131,6 +133,42 @@ int main(int argc, char* argv[]) {
         std::cout << "Clique solver result (UB): " << weighted_chromatic_n_ub << " (" << smwgcp_cliques::sum_of_weights(cwgraph) << " - " << max_clique_sol.lb << ")\n";
 
         ofs << "weighted-clique," << instance << "," << cwgraph << "," << weighted_chromatic_n_lb << "," << weighted_chromatic_n_ub << "," << elapsed << "\n";
+    } else if(parser["problem-type"].get().string == "weighted-stable-set") {
+        std::cout << "Reading graph from file...\n";
+
+        const auto cwgraph = smwgcp_cliques::read_clustered_weighted_graph(graph_file);
+        const auto working_graph = as::graph::complementary(cwgraph);
+
+        std::cout << "Graph read from file (" << duration_cast<duration<float>>(high_resolution_clock::now() - zero_time).count() << " s)\n";
+        std::cout << "Preparing Max-Weight Stable Set graph...\n";
+
+        const auto mwss_graph = smwgcp_cliques::sandwich_line_graph(working_graph);
+
+        std::cout << "Max-Weight Stable Set graph ready (" << duration_cast<duration<float>>(high_resolution_clock::now() - zero_time).count() << " s) ";
+        std::cout << boost::num_vertices(mwss_graph) << " vertices and " << boost::num_edges(mwss_graph) << " edges\n";
+        std::cout << "Launching the Max-Weight Stable Set solver (Sewell)...\n";
+
+        std::vector<std::uint32_t> weights(boost::num_vertices(mwss_graph));
+        for(auto v = 0u; v < boost::num_vertices(mwss_graph); ++v) {
+            weights[v] = static_cast<std::uint32_t>(mwss_graph[v].weight);
+        }
+
+        const auto start_time = high_resolution_clock::now();
+        const auto mwss_sol = as::mwis::mwis(weights, mwss_graph);
+        const auto end_time = high_resolution_clock::now();
+        const auto elapsed = duration_cast<duration<float>>(end_time - start_time).count();
+
+        std::cout << "Sewell Stable Set solver finished (" << elapsed << " s)\n";
+
+        const float stable_set_result = std::accumulate(mwss_sol.begin(), mwss_sol.end(), 0.0f,
+            [&] (float cum, auto v) -> float {
+                return cum + mwss_graph[v].weight;
+            }
+        );
+        const auto weighted_chromatic_n = smwgcp_cliques::sum_of_weights(cwgraph) - stable_set_result;
+
+        std::cout << "Stable Set solver result: " << weighted_chromatic_n << " (" << smwgcp_cliques::sum_of_weights(cwgraph) << " - " << stable_set_result << ")\n";
+        ofs << "weighted-stable-set," << instance << "," << cwgraph << "," << weighted_chromatic_n << "," << elapsed << "\n";
     } else if(parser["problem-type"].get().string == "weighted-mip") {
         std::cout << "Reading graph from file...\n";
 
